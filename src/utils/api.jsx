6f95830d8ebbc37a053b1f619097926cac07be83
maluki65 <brayanmaluki@gmail.com>
2025-09-22 11:53:47 +0300

    Changed backend Axio API

--- src/utils/api.jsx
+++ src/utils/api.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/v1/api/',
+  baseURL: 'https://kodianic-backend.onrender.com/v1/api/',
   withCredentials: true,
 });
 
